3c825d7ba98f395560f6a1632b747b28ff9ac9fd
emman-pip <parrenoemmanuel756@gmail.com>
2023-11-25 17:28:11 +0800

    Add launch the application prompt.

---
+++ src/mainFile.java
@@ -0,0 +1,39 @@
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+public class mainFile extends JFrame {
+    JFrame frm = this;
+
+    mainFile() {
+        new flatlaf();
+        JPanel pnl = new JPanel();
+        JButton btn = new JButton("START APP");
+        pnl.setLayout(new GridBagLayout());
+        pnl.add(btn);
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frm.dispose();
+                new landingWIndow();
+            }
+        });
+        this.add(pnl);
+        this.setSize(300, 300);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new mainFile();
+    }
+}
+
+// TODO:
+// AYUSIN YUNG ADD RECORDS SA ADMIN WINDOW
+// DESIGN THE FRONT END!!!!
+// SYSTEMATIC DELETE
